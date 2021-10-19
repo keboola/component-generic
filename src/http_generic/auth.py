7f57008783101d90ba59d606b2b94c1563e2ab11
David Esner <david@keboola.com>
2021-10-19 15:35:54 +0200

    Auth methods support, basic auth

--- src/http_generic/auth.py
+++ src/http_generic/auth.py
@@ -1,10 +1,19 @@
+import inspect
 from abc import ABC, abstractmethod
-from typing import Callable, Union
+from typing import Callable, Union, Dict
 
 from requests.auth import AuthBase, HTTPBasicAuth
 
 
+class AuthBuilderError(Exception):
+    pass
+
+
 class AuthMethodBase(ABC):
+    """
+    Base class to implement the authentication method. To mark secret constructor parameters prefix them with __
+    e.g. __init__(self, username, __password)
+    """
 
     @abstractmethod
     def login(self):
@@ -18,30 +27,64 @@ class AuthMethodBase(ABC):
 class AuthMethodBuilder:
 
     @classmethod
-    def build(cls, method_name, **parameters):
-        # TODO: validate parameters based on type
-        supported_actions = cls.get_methods_actions()
+    def build(cls, method_name: str, **parameters):
+        """
+
+        Args:
+            method_name:
+            **parameters: dictionary of named parameters. Note that parameters prefixed # will be converted to __
+
+        Returns:
+
+        """
+        supported_actions = cls.get_methods()
+
+
         if method_name not in list(supported_actions.keys()):
-            raise ValueError(f'{method_name} is not supported auth method, '
-                             f'supported values are: [{list(supported_actions.keys())}]')
+            raise AuthBuilderError(f'{method_name} is not supported auth method, '
+                                   f'supported values are: [{list(supported_actions.keys())}]')
+        parameters = cls._convert_secret_parameters(supported_actions[method_name], ** parameters)
+        cls._validate_method_arguments(supported_actions[method_name], **parameters)
 
         return supported_actions[method_name](**parameters)
 
     @staticmethod
-    def get_methods_actions():
+    def _validate_method_arguments(method: object, **args):
+        class_prefix = f"_{method.__name__}__"
+        arguments = [p for p in inspect.signature(method.__init__).parameters if p != 'self']
+        missing_arguments = []
+        for p in arguments:
+            if p not in args:
+                missing_arguments.append(p.replace(class_prefix, '#'))
+        if missing_arguments:
+            raise AuthBuilderError(f'Some arguments of method {method.__name__} are missing: {missing_arguments}')
+
+    @staticmethod
+    def _convert_secret_parameters(method: object, **parameters):
+        new_parameters = {}
+        for p in parameters:
+            new_parameters[p.replace('#', f'_{method.__name__}__')] = parameters[p]
+        return new_parameters
+
+    @staticmethod
+    def get_methods() -> Dict[str, Callable]:
         supported_actions = {}
         for c in AuthMethodBase.__subclasses__():
             supported_actions[c.__name__] = c
         return supported_actions
+
+    @classmethod
+    def get_supported_methods(cls):
+        return list(cls.get_methods().keys())
 
 
 # ########### SUPPORTED AUTHENTICATION METHODS
 
 class BasicHttp(AuthMethodBase):
 
-    def __init__(self, username, password):
+    def __init__(self, username, __password):
         self.__username = username
-        self.__password = password
+        self.__password = __password
 
     def login(self) -> Union[AuthBase, Callable]:
         return HTTPBasicAuth(username=self.__username, password=self.__password)
