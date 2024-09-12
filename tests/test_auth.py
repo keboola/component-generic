0a420f84b906037c242f0fa1993321036f9d4a9b
Josef Kudera <46950237+kudj@users.noreply.github.com>
2024-09-12 11:23:51 +0200

    fix tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,6 +2,7 @@ import unittest
 from pathlib import Path
 
 from http_generic.auth import AuthMethodBuilder, AuthBuilderError, BasicHttp
+from configuration import WriterConfiguration, ApiConfig, Authentication, ApiRequest, RequestContent
 
 
 class TestConfiguration(unittest.TestCase):
@@ -16,15 +17,35 @@ class TestConfiguration(unittest.TestCase):
 
     def test_invalid_method_params_fail(self):
         params = {'#password': 'test'}
-        with self.assertRaises(AuthBuilderError):
-            AuthMethodBuilder.build('BasicHttp', **params)
+
+        writer_conf = WriterConfiguration(
+            api=ApiConfig(authentication=Authentication(type='Login'), base_url=''),
+            request_parameters=ApiRequest(method='GET', endpoint_path=''),
+            request_content=RequestContent(content_type='json')
+        )
+
+        with self.assertRaises(ValueError):
+            AuthMethodBuilder.build(writer_conf, **params)
 
     def test_invalid_method_fail(self):
+        writer_conf = WriterConfiguration(
+            api=ApiConfig(authentication=Authentication(type='INVALID'), base_url=''),
+            request_parameters=ApiRequest(method='GET', endpoint_path=''),
+            request_content=RequestContent(content_type='json')
+        )
+
         with self.assertRaises(AuthBuilderError):
-            AuthMethodBuilder.build('INVALID', **{})
+            AuthMethodBuilder.build(writer_conf, **{})
 
     def test_valid_method_params_pass(self):
         params = {'username': "usr", '#password': 'test'}
         expected = BasicHttp(username='usr', _BasicHttp__password='test')
-        auth_method = AuthMethodBuilder.build('BasicHttp', **params)
+
+        writer_conf = WriterConfiguration(
+            api=ApiConfig(authentication=Authentication(type='BasicHttp'), base_url=''),
+            request_parameters=ApiRequest(method='GET', endpoint_path=''),
+            request_content=RequestContent(content_type='json')
+        )
+
+        auth_method = AuthMethodBuilder.build(writer_conf, **params)
         self.assertEqual(expected, auth_method)
